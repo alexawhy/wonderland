055426052a69802169bf23855c2d7687c43defda
Alexandria Wong <alexandriawhy@gmail.com>
2020-12-13 05:01:13 -0500

    Add clouds to parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,6 +1,8 @@
 const bush1 = document.getElementById("bush1");
 const bush2 = document.getElementById("bush2");
-const sky = document.getElementById("sky");
+const cloud1 = document.getElementById("cloud1");
+const cloud2 = document.getElementById("cloud2");
+const cloud3 = document.getElementById("cloud3");
 const title = document.getElementById("title");
 const rabbithole = document.getElementById("rabbithole");
 
@@ -16,9 +18,14 @@ document.addEventListener("scroll", (e) => {
   bush2.style.right = `${-scroll/20}%`;
   bush2.style.width= `${60 + scroll/40}%`;
 
-  sky.style.top = `${20 -scroll/50}%`;
-
   rabbithole.style.width = `${120 + scroll/40}%`;
 
   title.style.top = `${scroll/30}%`;
+
+  cloud1.style.top = `${5 - scroll/20}%`;
+  cloud1.style.width = `${15 - scroll/80}%`;
+  cloud2.style.top = `${20 - scroll/15}%`;
+  cloud2.style.width = `${15 - scroll/80}%`;
+  cloud3.style.top = `${40 - scroll/10}%`;
+  cloud3.style.width = `${17.5 - scroll/80}%`;
 })
