a4288f2ba9373bd367c0a75b8ba0869469ab99fb
Alexandria Wong <alexandriawhy@gmail.com>
2020-12-23 00:45:38 -0500

    Add parallax effect for dark mode

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -4,8 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const cloud1 = document.getElementById("cloud1");
   const cloud2 = document.getElementById("cloud2");
   const cloud3 = document.getElementById("cloud3");
+  const star1 = document.getElementById("star1");
+  const star2 = document.getElementById("star2");
+  const star3 = document.getElementById("star3");
+  const star4 = document.getElementById("star4");
   const title = document.getElementById("title");
-  const plus1 = document.getElementById("plus-1");
+  const plusParallax = document.getElementById("plus-parallax");
+  const plusStar = document.getElementById("plus-star");
   const rabbithole = document.getElementById("rabbithole");
 
   let scroll = window.pageYOffset;
@@ -23,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
     rabbithole.style.width = `${120 + scroll/15}%`;
 
     title.style.top = `${scroll/30}%`;
-    plus1.style.top = `${5 + scroll/30}%`;
+    plusParallax.style.top = `${5 + scroll/30}%`;
   
     cloud1.style.top = `${5 - scroll/20}%`;
     cloud1.style.left = `${3 - scroll/80}%`;
@@ -34,6 +39,12 @@ document.addEventListener("DOMContentLoaded", () => {
     cloud3.style.top = `${40 - scroll/10}%`;
     cloud3.style.left = `${15 - scroll/60}%`;
     cloud3.style.width = `${17.5 - scroll/80}%`;
+
+    star1.style.top = `${5 - scroll/20}%`;
+    star2.style.top = `${10 - scroll/20}%`;
+    star3.style.top = `${30 - scroll/20}%`;
+    star4.style.top = `${40 - scroll/20}%`;
+    plusStar.style.top = `${10 - scroll/20}%`;
   })
 })
 
