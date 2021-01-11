a7b038aa4eb767b32ed439df67196c2d5a0478e4
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-11 02:14:19 -0500

    Add description

--- src/scripts/cheshirecat.js
+++ src/scripts/cheshirecat.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
       let radian = Math.atan2(event.pageX - x, event.pageY - y);
       let rotation = (radian * (180 * Math.PI) * -1) + 270;
       eye.style.transform = `rotate(${rotation/120}deg)`
+      // let x = event.pageX;
+      // let y = event.pageY;
+      // eye.style.transform = `translate(${x / 100}}em, ${y / 100}em)`
     })
   })
 });
