8c2d4828e19c3275d988a9b97e32d8261d0d64f9
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-15 00:28:33 -0500

    Add limit to drag and drop

--- src/scripts/drag-and-drop.js
+++ src/scripts/drag-and-drop.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".drag-and-drop");
   const alexContainer = document.querySelector(".alex");
   const alex = document.querySelector(".alex img");
-  const shrink = document.querySelector(".shrink img");
-  const enlarge = document.querySelector(".enlarge img");
 
-  shrink.innerHTML = "Drink Me";
-  enlarge.innerHTML = "Eat Me";
+  const shrink = document.querySelector(".shrink");
+  const newShrinkPrompt = document.createElement("p");
+  newShrinkPrompt.innerHTML = "Drink Me!";
+  let newShrinkImg = document.createElement("img");
+  newShrinkImg.setAttribute("class", "draggable");
+  newShrinkImg.setAttribute("draggable", "true");
+
+  const enlarge = document.querySelector(".enlarge");
+  const newEnlargePrompt = document.createElement("p");
+  newEnlargePrompt.innerHTML = "Eat Me!";
+  let newEnlargeImg = document.createElement("img");
+  newEnlargeImg.setAttribute("class", "draggable");
+  newEnlargeImg.setAttribute("draggable", "true");
+
+  let shrinkCount = 4;
+  let enlargeCount = 4;
 
   shrink.addEventListener("dragstart", ev => {
     ev.dataTransfer.setData("text/plain", "0.5");
@@ -28,5 +39,77 @@ document.addEventListener("DOMContentLoaded", () => {
     let scale = parseFloat(ev.dataTransfer.getData("text/plain"));
     let newScale = originalScale * scale;
     alex.style.transform = `scale(${newScale})`;
+  
+    if (scale === 0.5) {
+      shrinkCount -= 1;
+      switch (shrinkCount) {
+        case 3:
+          shrink.removeChild(document.querySelector(".shrink img"));
+          shrink.removeChild(document.querySelector(".shrink p"));
+          newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-3.svg")
+          shrink.appendChild(newShrinkImg);
+          shrink.appendChild(newShrinkPrompt);
+          break;
+        case 2:
+          shrink.removeChild(document.querySelector(".shrink img"));
+          shrink.removeChild(document.querySelector(".shrink p"));
+          newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-2.svg")
+          shrink.appendChild(newShrinkImg);
+          shrink.appendChild(newShrinkPrompt);
+          break;
+        case 1:
+          shrink.removeChild(document.querySelector(".shrink img"));
+          shrink.removeChild(document.querySelector(".shrink p"));
+          newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-1.svg")
+          shrink.appendChild(newShrinkImg);
+          shrink.appendChild(newShrinkPrompt);
+          break;
+        case 0:
+          shrink.removeChild(document.querySelector(".shrink img"));
+          shrink.removeChild(document.querySelector(".shrink p"));
+          newShrinkImg.setAttribute("draggable", "false")
+          newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-0.svg")
+          const end = document.createElement("p");
+          end.innerHTML = "No more potion :("; 
+          shrink.appendChild(newShrinkImg);
+          shrink.appendChild(end);
+          break;
+      }
+    } else {
+      enlargeCount -= 1;
+      switch (enlargeCount) {
+        case 3:
+          enlarge.removeChild(document.querySelector(".enlarge img"));
+          enlarge.removeChild(document.querySelector(".enlarge p"));
+          newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-3.svg")
+          enlarge.appendChild(newEnlargeImg);
+          enlarge.appendChild(newEnlargePrompt);
+          break;
+        case 2:
+          enlarge.removeChild(document.querySelector(".enlarge img"));
+          enlarge.removeChild(document.querySelector(".enlarge p"));
+          newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-2.svg")
+          enlarge.appendChild(newEnlargeImg);
+          enlarge.appendChild(newEnlargePrompt);
+          break;
+        case 1:
+          enlarge.removeChild(document.querySelector(".enlarge img"));
+          enlarge.removeChild(document.querySelector(".enlarge p"));
+          newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-1.svg")
+          enlarge.appendChild(newEnlargeImg);
+          enlarge.appendChild(newEnlargePrompt);
+          break;
+        case 0:
+          enlarge.removeChild(document.querySelector(".enlarge img"));
+          enlarge.removeChild(document.querySelector(".enlarge p"));
+          newEnlargeImg.setAttribute("draggable", "false")
+          newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-0.svg")
+          const end = document.createElement("p");
+          end.innerHTML = "No more cookie :("; 
+          enlarge.appendChild(newEnlargeImg);
+          enlarge.appendChild(end);
+          break;
+      }
+    }
   })
 })
