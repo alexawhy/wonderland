190c8a201d27b344c9fabf80d22e223d378ea14a
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-15 02:37:57 -0500

    Dry up drag and drop function

--- src/scripts/drag-and-drop.js
+++ src/scripts/drag-and-drop.js
@@ -42,74 +42,50 @@ document.addEventListener("DOMContentLoaded", () => {
   
     if (scale === 0.5) {
       shrinkCount -= 1;
+      shrink.removeChild(document.querySelector(".shrink img"));
+      shrink.removeChild(document.querySelector(".shrink p"));
       switch (shrinkCount) {
         case 3:
-          shrink.removeChild(document.querySelector(".shrink img"));
-          shrink.removeChild(document.querySelector(".shrink p"));
           newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-3.svg")
-          shrink.appendChild(newShrinkImg);
-          shrink.appendChild(newShrinkPrompt);
           break;
         case 2:
-          shrink.removeChild(document.querySelector(".shrink img"));
-          shrink.removeChild(document.querySelector(".shrink p"));
           newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-2.svg")
-          shrink.appendChild(newShrinkImg);
-          shrink.appendChild(newShrinkPrompt);
           break;
         case 1:
-          shrink.removeChild(document.querySelector(".shrink img"));
-          shrink.removeChild(document.querySelector(".shrink p"));
           newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-1.svg")
-          shrink.appendChild(newShrinkImg);
-          shrink.appendChild(newShrinkPrompt);
           break;
         case 0:
-          shrink.removeChild(document.querySelector(".shrink img"));
-          shrink.removeChild(document.querySelector(".shrink p"));
           newShrinkImg.setAttribute("draggable", "false")
           newShrinkImg.setAttribute("src", "src/assets/drag-and-drop/potion-0.svg")
-          const end = document.createElement("p");
-          end.innerHTML = "No more potion :("; 
-          shrink.appendChild(newShrinkImg);
-          shrink.appendChild(end);
           break;
       }
+      shrink.appendChild(newShrinkImg);
+      const shrinkEnd = document.createElement("p");
+      shrinkEnd.innerHTML = "No more potion :("; 
+      shrinkCount === 0 ? shrink.appendChild(shrinkEnd) : shrink.appendChild(newShrinkPrompt);
     } else {
       enlargeCount -= 1;
+      enlarge.removeChild(document.querySelector(".enlarge img"));
+      enlarge.removeChild(document.querySelector(".enlarge p"));
       switch (enlargeCount) {
         case 3:
-          enlarge.removeChild(document.querySelector(".enlarge img"));
-          enlarge.removeChild(document.querySelector(".enlarge p"));
           newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-3.svg")
-          enlarge.appendChild(newEnlargeImg);
-          enlarge.appendChild(newEnlargePrompt);
           break;
         case 2:
-          enlarge.removeChild(document.querySelector(".enlarge img"));
-          enlarge.removeChild(document.querySelector(".enlarge p"));
           newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-2.svg")
-          enlarge.appendChild(newEnlargeImg);
-          enlarge.appendChild(newEnlargePrompt);
           break;
         case 1:
-          enlarge.removeChild(document.querySelector(".enlarge img"));
-          enlarge.removeChild(document.querySelector(".enlarge p"));
           newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-1.svg")
-          enlarge.appendChild(newEnlargeImg);
-          enlarge.appendChild(newEnlargePrompt);
           break;
         case 0:
-          enlarge.removeChild(document.querySelector(".enlarge img"));
-          enlarge.removeChild(document.querySelector(".enlarge p"));
           newEnlargeImg.setAttribute("draggable", "false")
           newEnlargeImg.setAttribute("src", "src/assets/drag-and-drop/cookie-0.svg")
-          const end = document.createElement("p");
-          end.innerHTML = "No more cookie :("; 
-          enlarge.appendChild(newEnlargeImg);
-          enlarge.appendChild(end);
           break;
       }
+      enlarge.appendChild(newEnlargeImg);
+      const enlargeEnd = document.createElement("p");
+      enlargeEnd.innerHTML = "No more cookie :("; 
+      enlargeCount === 0 ? enlarge.appendChild(enlargeEnd) : enlarge.appendChild(newEnlargePrompt);
     }
   })
 })
